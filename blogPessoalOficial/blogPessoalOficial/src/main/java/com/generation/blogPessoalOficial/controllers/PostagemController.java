b4eb32f23345cef4b50f9d2413fdf29033d58915
DanielSanm <danielelias3221@gmail.com>
2021-10-07 19:23:38 -0300

    [Author Daniel - Task: Corrigindo repositorios]

--- blogPessoalOficial/blogPessoalOficial/src/main/java/com/generation/blogPessoalOficial/controllers/PostagemController.java
+++ blogPessoalOficial/blogPessoalOficial/src/main/java/com/generation/blogPessoalOficial/controllers/PostagemController.java
@@ -6,11 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,28 +47,6 @@ public class PostagemController {
 			return ResponseEntity.status(204).build();
 		} else {
 			return ResponseEntity.status(200).body(lista);
-		}
-	}
-	
-	@PostMapping("/salvar")
-	public ResponseEntity<PostagemModel> salvar(@RequestBody PostagemModel postagem) {
-		return ResponseEntity.status(201).body(repositorio.save(postagem));
-	}
-	
-	@PutMapping("/atualizar")
-	public ResponseEntity<PostagemModel> atualizar(@RequestBody PostagemModel postagem) {
-		return ResponseEntity.status(201).body(repositorio.save(postagem));
-	}
-	
-	@DeleteMapping("/deletar/{id_postagem}")
-	public ResponseEntity<PostagemModel> deletar(@PathVariable(value = "id_postagem") long idPostagem) {
-		Optional<PostagemModel> optional = repositorio.findById(idPostagem);
-		
-		if(optional.isPresent()) {
-			repositorio.deleteById(idPostagem);
-			return ResponseEntity.status(201).build();
-		} else {
-			return ResponseEntity.status(404).build();
 		}
 	}
 }
